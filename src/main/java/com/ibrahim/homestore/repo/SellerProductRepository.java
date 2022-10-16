967e2addba5b943813b9e73aafebaca245c35d7b
ibrahim4851 <erogluih2001@gmail.com>
2022-10-16 11:48:26 +0300

    update

--- src/main/java/com/ibrahim/homestore/repo/SellerProductRepository.java
+++ src/main/java/com/ibrahim/homestore/repo/SellerProductRepository.java
@@ -4,6 +4,8 @@ import com.ibrahim.homestore.entity.SellerProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SellerProductRepository extends JpaRepository<SellerProduct, Long> {
 
